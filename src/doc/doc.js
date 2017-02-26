0565b8a793f3393727d2c7026b3f83700af42b09
abdulaziz alaboudi <abdulaziz alaboudi>
2017-02-26 10:30:39 +0300

    add FAQ

---
+++ src/doc/doc.js
@@ -0,0 +1,36 @@
+import $ from 'bootstrap';
+import { EventAggregator } from 'aurelia-event-aggregator';
+import { inject } from 'aurelia-framework';
+@inject(EventAggregator)
+export class Doc {
+  constructor(eventAggregator) {
+    this.event = eventAggregator;
+    this.TDD = `
+    
+    function add (x,y) {
+            //TODO
+    }`;
+    this.function = `
+
+    function add (x,y) {
+        return x+y
+    }`;
+  }
+
+  generateTDD() {
+    this.publish('onCodeChangeRequest', {code: this.TDD});
+  }
+  generateFunction() {
+    this.publish('onCodeChangeRequest', {code: this.function});
+  }
+
+  publish(event, publishPayload) {
+    switch (event) {
+    case 'onCodeChangeRequest':
+      this.event.publish('onCodeChangeRequest', publishPayload);
+      break;
+    default:
+      break;
+    }
+  }
+}
